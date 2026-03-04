31f31a4a4fc02c8598931b5b3d708496366586f2
Daniele Martinoli <dmartino@redhat.com>
2026-03-04 07:57:37 +0100

    formatting
    Signed-off-by: Daniele Martinoli <dmartino@redhat.com>

--- tests/test_marketplace_model.py
+++ tests/test_marketplace_model.py
@@ -127,9 +127,7 @@ class TestMarketplaceFromUrl:
         """Load marketplace from file:// URL."""
         market_file = tmp_path / "market.yml"
         market_file.write_text(
-            "name: File URL Marketplace\n"
-            "version: 1.0.0\n"
-            "modules: []\n"
+            "name: File URL Marketplace\nversion: 1.0.0\nmodules: []\n"
         )
         file_url = market_file.as_uri()
         marketplace = Marketplace.from_url(file_url, "file-market")
