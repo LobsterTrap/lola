d40ad214a237827367d027717f9a909b0549fdf1
Igor Brandao <ibrandao@redhat.com>
2026-06-11 12:32:42 -0300

    docs(modules): add package and function doc comments, TODO return types

--- internal/modules/info.go
+++ internal/modules/info.go
@@ -1,5 +1,7 @@
 package modules
 
+// Info returns detailed information about the named module.
+// TODO: return (*Module, error) once the Module type is defined in pkg/models.
 func Info(name string) error {
 	return ErrNotImplemented
 }
